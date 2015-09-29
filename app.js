bba0e7e0000d2fcbb87b6c1dac381576523da957
YOUR NAME <SEB@MacBook-Air-de-Sebastien.local>
2015-09-29 11:57:28 +0200

    version admin.html

--- app.js
+++ app.js
@@ -8,8 +8,15 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/admin', function(req, res){
+  res.sendFile(__dirname + '/admin.html');
+});
+
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('message', function (body) {
+    console.log(body);
+  });
 });
 
 http.listen(3000);
